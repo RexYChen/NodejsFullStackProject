13d3f4f2f41d2e8f3fc68a99fb0062197e0ace6a
RexChen <RexChen@Rex-MacBook-Pro.local>
2018-03-30 23:51:31 +1300

    finish the first version

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -4,9 +4,8 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.get('/', function(req, res){
   knex('todo').select().then(todos =>{
-
     res.render('all', { todos: todos});
   });
 });
@@ -15,25 +14,72 @@ router.get('/new', (req, res) => {
     res.render('new');
 });
 
+router.get('/:id', function(req, res){
+  const id = req.params.id;
+  if(typeof id != 'undefined'){
+    knex('todo').select()
+    .where('id',id)
+    .first()
+    .then(todo =>{
+      console.log(todo);
+      res.render('single', { todo: todo});
+    });
+  }else{
+    es.status(500);
+    res.render('error',{
+      message:'invalid id'
+    });
+  }
+});
+
+router.get('/:id/edit',(req,res)=>{
+  const id = req.params.id;
+  if(typeof id != 'undefined'){
+    knex('todo').select()
+    .where('id',id)
+    .first()
+    .then(todo =>{
+      console.log(todo);
+      res.render('edit',todo);
+    });
+  }else{
+    es.status(500);
+    res.render('error',{
+      message:'invalid id'
+    });
+  }
+});
+
 function vaildTodo(todo)
 {
+  console.log(typeof todo.title,typeof todo.priority);
   return typeof todo.title == 'string' &&
-         todo.title.trim() != '' && typeof todo.priority == 'number';
+         todo.title.trim() != '' && typeof todo.priority != 'undefined'
+        && !isNaN(Number(todo.priority));
 }
+
 router.post('/', (req, res) => {
 
     console.log(req.body);
 
-    // if(vaildTodo(req.body)){
-    //   const todo{
-    //     title:req.body.Title;
-    //     priority: req.body.priority;
-    //     done: req.body.;
-    //   }
-    // }else
-    // {
-    //
-    // }
+    if(vaildTodo(req.body)){
+      const todo ={
+        title:req.body.title,
+        priority: req.body.priority,
+        done: req.body.done,
+      };
+      knex('todo').returning('id').insert(todo).then(ids=>{
+        const id1 = ids[0];
+        console.log(id1);
+        res.redirect('/todo/${id}');
+      });
+    }else
+    {
+      res.status(500);
+      res.render('error',{
+        message:'invalid to do'
+      });
+    }
      res.render('new');
 });
 
